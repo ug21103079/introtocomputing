080e6accc0f83d69f4cd9d95ef4a636b494db120
ug21103079 <96766463+ug21103079@users.noreply.github.com>
2022-01-08 13:07:30 +0530

    Update asg1_CSE_21103079.py

--- asg1_CSE_21103079.py
+++ asg1_CSE_21103079.py
@@ -8,6 +8,7 @@ num3=int(input("Enter The 3rd Number:\n"))
 average=(num1+num2+num3)/3
 #print average of 3 numbers
 print("Average=",average)
+
 #Question 2
 print("Question 2")
 #enter the gross income and number of dependent
@@ -20,6 +21,7 @@ Taxable_income=float(Gross_income)-Standard_Deduction-Deduction_dependent
 Tax=(Taxable_income*20)/100
 #print the tax
 print("Tax=$"+str(Tax))
+
 #Question 3
 print("Question 3")
 #Enter The Required Fields
@@ -31,6 +33,7 @@ CGPA=float(input("Enter The Student CGPA:\n"))
 Student=[SID,Name,Gender,Course_Name,CGPA]
 #print list
 print(Student)
+
 #Question 4
 print("Question 4")
 #Enter the marks of 5 students
@@ -47,6 +50,7 @@ print("Before Sorting the list\n",list)
 list.sort()
 #print the list after sort
 print("After Sorting the list\n",list)
+
 #Question 5(a)
 print("Question 5(a)")
 Colour=['Red','Green','White','Black','Pink','Yellow']
@@ -54,6 +58,7 @@ Colour=['Red','Green','White','Black','Pink','Yellow']
 Colour.remove(Colour[3])
 #print the list
 print(Colour)
+
 #Question 5(b)
 print("Question 5(b)")
 Colour=['Red','Green','White','Black','Pink','Yellow']
