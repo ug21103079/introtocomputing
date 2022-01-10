7a4e86b293496aa6b4929db7fed866947554f826
ug21103079 <96766463+ug21103079@users.noreply.github.com>
2022-01-10 16:18:01 +0530

    Add files via upload

---
+++ asg1_CSE_21103079_output.py
@@ -0,0 +1,53 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\HP\Desktop\asg1_21103079\asg1_CSE_21103079.py =======
+Question 1
+Enter The 1st Number:
+45
+Enter The 2nd Number:
+42
+Enter The 3rd Number:
+54
+Average= 47.0
+Question 2
+Enter The Gross Income:
+$1000051
+Enter The Number Of Dependent:
+4
+Tax=$195610.2
+Question 3
+Enter The Student's SID:
+21103079
+Enter The Student's Name:
+Ujjawal Gupta
+The Student's Gender:
+Enter F for Female
+Enter M for Male
+Enter U if Gender is Unknown
+M
+Enter the Course taken by Student:
+CSE
+Enter The Student CGPA:
+8.4
+[21103079, 'Ujjawal Gupta', 'M', 'CSE', 8.4]
+Question 4
+Enter The Marks Of 1st Student:
+48
+Enter The Marks Of 2nd Student:
+84
+Enter The Marks Of 3rd Student:
+25
+Enter The Marks Of 4th Student:
+87
+Enter The Marks Of 5th Student:
+12
+Before Sorting the list
+ [48, 84, 25, 87, 12]
+After Sorting the list
+ [12, 25, 48, 84, 87]
+Question 5(a)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+Question 5(b)
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+>>> 
