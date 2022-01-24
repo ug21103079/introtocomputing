209139e12167f0766dab64377f58653df0a51096
ug21103079 <96766463+ug21103079@users.noreply.github.com>
2022-01-24 20:03:02 +0530

    Add files via upload

---
+++ asg2_21103079_CSE_Ujjawal Gupta(Output).py
@@ -0,0 +1,60 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: E:\Assignment PEC\asg2_21103079_CSE_Ujjawal Gupta(Code).py =====
+Question 1
+A Part
+35
+B Part
+egaugnal evitisnes esac a si nohtyP
+C Part
+a case sensitive
+D Part
+Python is object oriented language
+E Part
+10
+F Part
+Pythonisacasesensitivelanguage
+
+
+Question 2
+Hey, Ujjawal Gupta Here!
+My SID is 21103079
+I am from CSE department and my CGPA is 9.9
+
+
+Question 3
+a&b: 8
+a|b: 58
+a^b: 50
+Left Shift of a by 2 bits: 224 Left shift of b by 2 bits: 40
+Right Shift of a by 2 bits: 14 Right shift of b by 4 bits: 0
+
+
+Question 4
+Enter 1st Number:
+48
+Enter 2nd Number:
+54
+Enter 3rd Number:
+14
+The Greatest Number is:
+54
+
+
+Question 5
+Enter A String:
+hi my name is ujjawal
+Yes
+
+
+Question 6
+Enter The Side 1:
+14
+Enter The Side 2:
+15
+Enter The Side 3:
+7
+If the triangle can be formed?
+Yes
+>>> 
